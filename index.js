454d912328bbda149e9ddeb2b45e87f41f3e768a
muhammad abdullah <muhmmadabdullah6346@gmail.com>
2024-07-26 12:46:06 +0500

    feat(Data Fetching and Writing): In index.js crud opertions are performed
    I have fetched old data from my user table that was empty and then
    iwrite a new data with a post request on postman

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const { PrismaClient } = require("@prisma/client");
 const express = require("express");
 const app = express();
 const port = 3000;
-const prisma = PrismaClient();
+const prisma = new PrismaClient();
+app.use(express.json());
 
 app.get("/", async (req, res) => {
   const users = await prisma.user.findMany();
   res.json(users);
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const createdata = await prisma.user.create({ data: req.body });
+    res.json(createdata);
+  } catch (err) {
+    console.log(err);
+    res
+      .status(400)
+      .json({ message: "failed to crete data", error: err.message });
+  }
 });
 
 app.listen(port, () => {
